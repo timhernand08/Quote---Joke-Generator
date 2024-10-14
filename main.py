c381e4fecd1e4ae91f7348b1f0def69f437c3ea4
Timothy Hernandez <you@example.com>
2024-10-14 12:27:58 -0400

    First iteration

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests, json
+
+QUOTE_API = 'https://zenquotes.io/api/random'
+JOKE_API = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,explicit'
+
+#generates quote from api
+def get_quote():
+  response = requests.get(QUOTE_API)
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + "\r\n - " + json_data[0]['a']
+  return quote
+
+#generates jokes from an api
+def get_joke():
+  response = requests.get(JOKE_API)
+  json_data = json.loads(response.text)
+  try:#looks to see if there is a single joke and prints if there is
+    joke = json_data['joke']
+    return joke
+  except:
+    setup = json_data['setup']
+    delivery = json_data['delivery']
+  return setup + "\n" + delivery
+
+#print quote and joke when run
+quote = get_quote()
+joke = get_joke()
+print(quote + "\n" + joke + "\n")
+
+
+#asks the user if they want to print a new quote/joke
+user = input("Please enter if you want a new quote or joke. Or enter 'done': ")
+while(user != "done"):
+  if(user == "quote"):
+    quote = get_quote()
+    print(quote)
+  if(user == "joke"):
+    joke = get_joke()
+    print(joke)  
+  user = input("\nMake a new request: ")
